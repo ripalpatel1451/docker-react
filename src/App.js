999a79bbb78a6011d0650a9ea133b1fa0bb32699
ripalpatel1451 <ripalpatel1451@gmail.com>
2020-10-25 23:28:25 -0400

    changed app test

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello World IS CHANGING!
+          Good Night React Again and Again!
         </p>
         <a
           className="App-link"
